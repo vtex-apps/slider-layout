807f0c874b0ecbc324a6e6a536725e1c19c66cfa
victorhmp <victor2142@gmail.com>
2019-09-11 17:38:04 -0300

    Move SliderLayoutProps interface def into globals.d.ts

---
+++ react/typings/globals.d.ts
@@ -0,0 +1,25 @@
+interface SliderLayoutProps {
+  label?: string
+  showNavigationArrows: 'mobileOnly' | 'desktopOnly' | 'always' | 'never'
+  infinite: boolean
+  showPaginationDots: 'mobileOnly' | 'desktopOnly' | 'always' | 'never'
+  slideTransition?: {
+    /** Transition speed in ms */
+    speed: number
+    /** Transition delay in ms */
+    delay: number
+    timing: string
+  }
+  autoplay?: {
+    /** Timeout duration in ms */
+    timeout: number
+    stopOnHover?: boolean
+  }
+  navigationStep: number | 'page'
+  usePagination: boolean
+  itemsPerPage: {
+    desktop: number
+    tablet: number
+    phone: number
+  }
+}
