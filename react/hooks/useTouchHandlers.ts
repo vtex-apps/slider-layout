190e0fcc39cec4015a4e1758b9b58772fed9b2f9
victorhmp <victor2142@gmail.com>
2019-09-11 17:38:58 -0300

    Create `useTouchHandlers` hook

---
+++ react/hooks/useTouchHandlers.ts
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import { useSliderDispatch, useSliderState } from '../components/SliderContext'
+import { populateSlides } from '../utils/populateSlides'
+
+export const useTouchHandlers = () => {
+  const dispatch = useSliderDispatch()
+  const {
+    currentSlide,
+    slidesPerPage,
+    slideWidth,
+    totalItems,
+    navigationStep,
+    infinite,
+  } = useSliderState()
+  const [touchStartX, setTouchStartX] = useState(0)
+  const SWIPE_THRESHOLD = 50
+
+  const populate = (direction: 'left' | 'right') => {
+    const { nextSlides, nextPosition } = populateSlides(
+      direction,
+      currentSlide,
+      slidesPerPage,
+      slideWidth,
+      totalItems,
+      navigationStep,
+      infinite
+    )
+    dispatch({
+      type: 'slide',
+      payload: {
+        transform: nextPosition || 0,
+        currentSlide: nextSlides || 0,
+      },
+    })
+  }
+
+  const onTouchStart = (e: React.TouchEvent) => {
+    const startX = e.touches[0].clientX
+    setTouchStartX(startX)
+  }
+
+  const onTouchEnd = (e: React.TouchEvent) => {
+    const endX = e.changedTouches[0].clientX
+    const delta = endX - touchStartX
+    if (Math.abs(delta) > SWIPE_THRESHOLD) {
+      if (delta > 0) populate('left')
+      if (delta < 0) populate('right')
+    }
+    setTouchStartX(0)
+  }
+
+  return { onTouchEnd, onTouchStart }
+}
