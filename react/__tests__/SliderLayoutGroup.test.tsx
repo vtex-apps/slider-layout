556006b2ee8a038cdc1d72c87033c9903b0552ab
victorhmp <victor2142@gmail.com>
2020-08-10 17:07:00 -0300

    Update tests for SliderLayoutGroup

--- react/__tests__/SliderLayoutGroup.test.tsx
+++ react/__tests__/SliderLayoutGroup.test.tsx
@@ -38,14 +38,16 @@ test('should move all sliders in a group together', () => {
   const secondSlider = container.querySelector('#second-slider') as HTMLElement
 
   const nextArrow = getByLabelText(firstSlider, 'Next Slide')
+  const previousArrow = getByLabelText(secondSlider, 'Previous Slide')
 
+  // Go to the right
   fireEvent.click(nextArrow)
 
-  const visibleSlideFirstSlider: HTMLElement | null = firstSlider.querySelector(
+  let visibleSlideFirstSlider: HTMLElement | null = firstSlider.querySelector(
     '[aria-hidden=false]'
   )
 
-  const visibleSlideSecondSlider: HTMLElement | null = secondSlider.querySelector(
+  let visibleSlideSecondSlider: HTMLElement | null = secondSlider.querySelector(
     '[aria-hidden=false]'
   )
 
@@ -53,8 +55,26 @@ test('should move all sliders in a group together', () => {
     throw new Error('Could not find any visible slides')
   }
 
-  getByText(visibleSlideFirstSlider, 'Slider 1 Item 2')
-  getByText(visibleSlideSecondSlider, 'Slider 2 Item 2')
+  let secondSlide1 = getByText(visibleSlideFirstSlider, 'Slider 1 Item 2')
+  let secondSlide2 = getByText(visibleSlideSecondSlider, 'Slider 2 Item 2')
 
-  expect(true).toBe(true)
+  expect(secondSlide1).toBeDefined()
+  expect(secondSlide2).toBeDefined()
+
+  // Go to the left
+  fireEvent.click(previousArrow)
+
+  visibleSlideFirstSlider = firstSlider.querySelector('[aria-hidden=false]')
+
+  visibleSlideSecondSlider = secondSlider.querySelector('[aria-hidden=false]')
+
+  if (!visibleSlideFirstSlider || !visibleSlideSecondSlider) {
+    throw new Error('Could not find any visible slides')
+  }
+
+  secondSlide1 = getByText(visibleSlideFirstSlider, 'Slider 1 Item 1')
+  secondSlide2 = getByText(visibleSlideSecondSlider, 'Slider 2 Item 1')
+
+  expect(secondSlide1).toBeDefined()
+  expect(secondSlide2).toBeDefined()
 })
