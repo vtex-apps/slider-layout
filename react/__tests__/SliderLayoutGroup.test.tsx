3edf2be7e292ff42d5b6027cd23225864299c3b9
victorhmp <victor2142@gmail.com>
2020-08-10 14:13:00 -0300

    Implement tests for SliderLayoutGroup

--- react/__tests__/SliderLayoutGroup.test.tsx
+++ react/__tests__/SliderLayoutGroup.test.tsx
@@ -1,25 +1,30 @@
 import React from 'react'
-import { render, getByLabelText, getByText, fireEvent } from '@vtex/test-tools/react'
+import {
+  render,
+  getByLabelText,
+  getByText,
+  fireEvent,
+} from '@vtex/test-tools/react'
 import { useResponsiveValue } from 'vtex.responsive-values'
 
 import SliderLayoutGroup from '../SliderLayoutGroup'
 import SliderLayout from '../SliderLayout'
 
-const mockedUseResponsiveValue = useResponsiveValue as jest.Mock<() => number>
+const mockedUseResponsiveValue = useResponsiveValue as jest.Mock<number>
 
 mockedUseResponsiveValue.mockImplementation(() => 1)
 
 test('should move all sliders in a group together', () => {
-  const { debug, container } = render(
+  const { container } = render(
     <SliderLayoutGroup>
-      <div id="first-slide">
+      <div id="first-slider">
         <SliderLayout showPaginationDots="never">
           <div>Slider 1 Item 1</div>
           <div>Slider 1 Item 2</div>
           <div>Slider 1 Item 3</div>
         </SliderLayout>
       </div>
-      <div id="second-slide">
+      <div id="second-slider">
         <SliderLayout showPaginationDots="never">
           <div>Slider 2 Item 1</div>
           <div>Slider 2 Item 2</div>
@@ -29,20 +34,27 @@ test('should move all sliders in a group together', () => {
     </SliderLayoutGroup>
   )
 
-  const firstSlide = container.querySelector('#first-slide') as HTMLElement
-  const secondSlide = container.querySelector('#second-slide') as HTMLElement
+  const firstSlider = container.querySelector('#first-slider') as HTMLElement
+  const secondSlider = container.querySelector('#second-slider') as HTMLElement
 
-  const nextArrow = getByLabelText(firstSlide, 'Next Slide')
+  const nextArrow = getByLabelText(firstSlider, 'Next Slide')
 
   fireEvent.click(nextArrow)
 
-  const visibleSlideFirstSlider = firstSlide.querySelector('[aria-hidden=false]')
-  const visibleSlideSecondSlider = secondSlide.querySelector('[aria-hidden=false]')
+  const visibleSlideFirstSlider: HTMLElement | null = firstSlider.querySelector(
+    '[aria-hidden=false]'
+  )
+
+  const visibleSlideSecondSlider: HTMLElement | null = secondSlider.querySelector(
+    '[aria-hidden=false]'
+  )
+
+  if (!visibleSlideFirstSlider || !visibleSlideSecondSlider) {
+    throw new Error('Could not find any visible slides')
+  }
 
   getByText(visibleSlideFirstSlider, 'Slider 1 Item 2')
   getByText(visibleSlideSecondSlider, 'Slider 2 Item 2')
-
-  debug()
 
   expect(true).toBe(true)
 })
